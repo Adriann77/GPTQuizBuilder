9964c10b1239a3badb01fabfa42bef6bbb87794b
Adrian <klimasa98@gmail.com>
2024-03-09 16:12:25 +0100

    trying to understand how gpt api works, and work hard against limited requests per minute

--- src/components/Ai/Testowe.tsx
+++ src/components/Ai/Testowe.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react';
 import { OPEN_AI_KEY } from '../../../API/ApiKey';
 
-console.log(OPEN_AI_KEY);
-
 export const Testowe = () => {
 	const [response, setResponse] = useState<any>(null);
+	const [gptAnswer, setGptAnswer] = useState<any>('');
+	const [requestCount, setRequestCount] = useState<number>(0);
 
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
 				const apiKey = OPEN_AI_KEY;
-				const message = 'Jaka jest dzisiaj pogoda?';
+				const message =
+					'Stworz dla mnie quiz w dziedzinie biologi w experckim stopniu zaawansowania o długości 3 pytan, chce zeby były 4 pytania do kazdego zapytania z czego 1 prawidlowe, quiz ma byc po "';
 
 				const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
 					method: 'POST',
@@ -19,7 +20,7 @@ export const Testowe = () => {
 						Authorization: `Bearer ${apiKey}`,
 					},
 					body: JSON.stringify({
-						model: 'gpt-3.5-turbo', // Wybierz odpowiedni model
+						model: 'gpt-3.5-turbo',
 						messages: [
 							{
 								role: 'user',
@@ -31,19 +32,21 @@ export const Testowe = () => {
 
 				const data = await response.json();
 				setResponse(data);
-				console.log(data);
+				console.log(data.choices[0].message);
+			
 			} catch (error) {
 				console.error('Błąd:', error);
 			}
 		};
 
-		fetchData();
-	}, []);
+		const timer = setTimeout(() => {
+			fetchData();
+			setRequestCount(requestCount + 1);
+		}, 21000); // 21 sekund
 
-	return (
-		<div>
-			<h1>Odpowiedź od API:</h1>
-			{response && response.choices && response.choices.length > 0 && <p>{response.choices[0].message.content}</p>}
-		</div>
-	);
+		// Ustawia czyszczenie interwału
+		return () => clearTimeout(timer);
+	}, [requestCount]); // Dodaje requestCount jako zależność, aby wywoływać useEffect ponownie po każdym zakończonym zapytaniu
+
+	return <div>{gptAnswer && <div>{gptAnswer}</div>}</div>;
 };
--- src/views/MainPage/MainPage.tsx
+++ src/views/MainPage/MainPage.tsx
@@ -4,13 +4,15 @@ import { Testowe } from '../../components/Ai/Testowe';
 import { Form } from '../../components/Form/Form';
 import { Header } from '../../components/Header/Header';
 import { MainLayout } from '../../components/MainLayout/MainLayout';
+import { Quiz } from '../../components/Quiz/Quiz';
 
 export const MainPage = () => {
 	return (
 		<MainLayout>
 			<Header />
-			<Form />
-			<Testowe/>
+			<Testowe />
+			{/* <Quiz/> */}
+			{/* <Form /> */}
 		</MainLayout>
 	);
 };
