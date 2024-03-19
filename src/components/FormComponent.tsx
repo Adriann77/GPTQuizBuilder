ee00859fad89c3fa26fad9aecec75d08b85c7728
Adrian <klimasa98@gmail.com>
2024-03-19 17:18:33 +0100

    setup react-hook-form

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,42 +1,55 @@
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 export const FormComponent = () => {
-	const { register, handleSubmit } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		watch,
+	} = useForm({
+		defaultValues: {
+			genre: '',
+			difficult: '',
+			lenght: 10,
+		},
+	});
 
 	return (
 		<form
 			onSubmit={handleSubmit(data => {
-				console.log(data);
+				console.log(data.genre);
 			})}
 			className='flex gap-4 flex-col'>
 			<input
-				{...register('genre')}
+				{...register('genre', { required: 'this is required', maxLength: 10 })}
 				type='text'
 				placeholder='Type here'
 				className='input input-bordered input-primary w-full max-w-xs'
 			/>
+			<p>{errors.genre?.message}</p>
 			<select
+				defaultValue='chooseDiff'
+				defaultChecked='chooseDiff'
 				{...register('difficult')}
 				className='select select-primary w-full max-w-xs'>
 				<option
 					disabled
-					selected>
-					What is the best TV show?
+					value='chooseDiff'>
+					Wybierz poziom trudności
 				</option>
-				<option>Game of Thrones</option>
-				<option>Lost</option>
-				<option>Breaking Bad</option>
-				<option>Walking Dead</option>
+				<option value='basic'>Podstawowe pytania</option>
+				<option value='intermediate'>Średnio-zaawansowane</option>
+				<option value='expert'>Expert w dziedzinie</option>
 			</select>
 			<input
-				{...register('lenght')}
+				{...register('lenght', { min: 5, max: 15 })}
 				type='range'
-				min={0}
-				max='100'
-				// value='40'
+				min={5}
+				max={15}
 				className='range range-primary'
 			/>
-			<button type='submit'>costam</button>
+			<p>{watch('lenght')}</p>
+			<input type='submit' />
 		</form>
 	);
 };
