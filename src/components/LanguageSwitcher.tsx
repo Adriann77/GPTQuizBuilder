05791ae20d87f2130784629ff1f5d9166ffa7b9f
Adrian <klimasa98@gmail.com>
2024-03-26 17:24:51 +0100

    update readme

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,7 +1,8 @@
 export const LanguageSwitcher = () => {
+    
 	return (
 		<div className='absolute top-[10%] left-5 flex gap-3 text-xs items-center'>
-			<p>POL</p>
+			<p>PL</p>
 			<input
 				type='checkbox'
 				className=' toggle [--tglbg:black] bg-primary hover:bg-primary border-primary'
