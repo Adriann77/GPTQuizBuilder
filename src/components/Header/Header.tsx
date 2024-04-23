a1fe29beef7bb6f41a16f952ac56e0094cbe4f6a
Adrian <klimasa98@gmail.com>
2024-04-23 18:04:55 +0200

    add some css to landing page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,35 +4,27 @@ import { Link } from 'react-router-dom';
 export default function Header() {
 	return (
 		<>
-			<header className=' w-screen min-h-[91vh] '>
+			<header className=' w-screen  '>
 				<Wrapper>
-					<div className='flex flex-col w-[100%] h-[100%] '>
-						<div className='flex flex-col  gap-3 items-center justify-center p-4 mt-[10vh] mb-[5vh]'>
-							<h1 className='text-xl '>Witaj w GPT-QuizBuilder</h1>
-							<p className='text-sm text-center'>
-								GPT-QuizBuilder to innowacyjna aplikacja edukacyjna, która wykorzystuje potężne możliwości sztucznej
-								inteligencji, w tym API ChatGPT, aby umożliwić Ci szybkie i łatwe tworzenie quizów dostosowanych do
-								Twoich potrzeb.
-							</p>
-						</div>
+					<div className='flex flex-col mb-5 mt-5 p-2 gap-3 '>
+						<h1 className='text-xl  text-center'>Witaj w GPT-QuizBuilder</h1>
 
+						<p className='text-sm text-center mt-4'>
+							GPT-QuizBuilder to innowacyjna aplikacja edukacyjna, która wykorzystuje potężne możliwości sztucznej
+							inteligencji, w tym API ChatGPT, aby umożliwić Ci szybkie i łatwe tworzenie quizów dostosowanych do Twoich
+							potrzeb.
+						</p>
+
+						<Link
+							to={'/form'}
+							className=' btn btn-primary   max-w-[300px] my-2 mx-auto'>
+							Stwórz swój Quiz
+						</Link>
 						<div className='flex flex-col  h-[100%] items-center justify-center text-sm'>
 							<h2 className='text-lg'>Dlaczego warto używać GPT-QuizBuilder?</h2>
 							<ul className='flex flex-wrap gap-2 mt-4 text-center'>
 								<li className='w-[48%]'>
-									<div className='flex flex-col gap-4 w-52'>
-										<div className='flex gap-4 items-center'>
-											<div className='skeleton w-16 h-16 rounded-full shrink-0'>
-												{' '}
-												<i className='fa-solid fa-puzzle-piece m-2'></i>
-											</div>
-											<div className='flex flex-col gap-4'> 
-												<div className='skeleton h-4 w-20'>personalizacja bez granic</div>
-
-											</div>
-										</div>
-										<div className='skeleton h-32 w-full'></div>
-									</div>
+									<i className='fa-solid fa-puzzle-piece m-2'></i>Personalizacja bez granic
 								</li>
 								<li className='w-[48%]'>
 									<i className='fa-regular fa-object-group m-2'></i> Prosty interfejs użytkownika
@@ -47,11 +39,6 @@ export default function Header() {
 								</li>
 							</ul>
 						</div>
-						<Link
-							to={'/form'}
-							className=' btn btn-primary mt-[5vh] '>
-							Stwórz swój Quiz
-						</Link>
 					</div>
 				</Wrapper>
 			</header>
