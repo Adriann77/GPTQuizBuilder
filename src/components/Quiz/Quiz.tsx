21af740c5dd1ea8f2d06a07d55f3582931eb0e74
Adrian <klimasa98@gmail.com>
2024-05-04 03:02:39 +0200

    connect modal w/openaiApi

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Summary } from '../Summary/Summary';
+import axios from 'axios';
 import { useTranslation } from 'react-i18next';
+const OPEN_AI_KEY = import.meta.env.VITE_OPEN_AI_KEY;
 
 interface Question {
   content: string;
@@ -30,6 +32,46 @@ export const Quiz = ({ questions, restartQuiz }: QuizProps) => {
   const question = questions[currentQuestionIndex];
 
   const { t } = useTranslation();
+
+
+  // Przygotowanie danych zapytania
+  const data = {
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content: 'You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.',
+      },
+      {
+        role: 'user',
+        content: 'Compose a poem that explains the concept of recursion in programming.',
+      },
+    ],
+  };
+
+  // Przygotowanie nagłówków
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization:  `Bearer ${OPEN_AI_KEY}`
+  };
+
+  // Wykonanie zapytania POST
+
+
+  const showModal = () => {
+    //@ts-ignore
+    document.getElementById('my_modal_2').showModal();
+      axios
+        .post('https://api.openai.com/v1/chat/completions', data, { headers: headers })
+        .then((response) => {
+          console.log('Response:', response.data);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+
+
+  };
 
   const handleAnswerSelect = (answer: string) => {
     setSelectedAnswer(answer);
@@ -83,11 +125,20 @@ export const Quiz = ({ questions, restartQuiz }: QuizProps) => {
           {showExplanation && (
             <>
               <div className="relative">
-                <p className="flex flex-col gap-2 text-md mx-auto my-4 w-[300px] rounded bg-[#1d1e20] p-3 text-center text-white lg:w-[400px] lg:text-xl">
+                <p className="text-md mx-auto my-4 flex w-[300px] flex-col gap-2 rounded bg-[#1d1e20] p-3 text-center text-white lg:w-[400px] lg:text-xl">
                   {question.description_correct}
-                  <button className="  btn btn-primary btn-outline ">
+
+                  <button className="btn btn-outline btn-primary" onClick={showModal}>
                     Extend
                   </button>
+                  <dialog id="my_modal_2" className="modal">
+                    <div className="modal-box">
+                      <p className="py-4">Press ESC key or click outside to close</p>
+                    </div>
+                    <form method="dialog" className="modal-backdrop">
+                      <button>close</button>
+                    </form>
+                  </dialog>
                 </p>
               </div>
 
