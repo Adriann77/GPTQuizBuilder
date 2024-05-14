cd74d286776ccfd425c500fa31206758d7828d80
Adrian <klimasa98@gmail.com>
2024-05-14 06:20:32 +0200

    add validaton for explanation

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Summary } from '../Summary/Summary';
 import axios from 'axios';
 import { useTranslation } from 'react-i18next';
+
 const OPEN_AI_KEY = import.meta.env.VITE_OPEN_AI_KEY;
 
 interface Question {
@@ -41,11 +42,11 @@ export const Quiz = ({ questions, restartQuiz }: QuizProps) => {
       {
         role: 'system',
         content:
-          'You are a knowledgeable teacher tasked with providing direct and clear answers without any introductory phrases or filler words. Your responses should be straightforward, focusing solely on the content requested by the user. Use examples if necessary to clarify your points but keep the explanations concise. Avoid numbering your explanations, but you may use numbered examples when necessary to clarify points. Do not use code block syntax for examples, use plain text instead. Do not try to make text bolder using "**bold text**" instead of this you can use dot or number. If u give an examples remeber to use double plain text before and after. At the end you can use some comprasions to explain in easier language with double plain text before',
+          'You are a knowledgeable teacher tasked with providing direct and clear answers without any introductory phrases or filler words. Your responses should be straightforward, focusing solely on the content requested by the user. Use examples if necessary to clarify your points but keep the explanations concise. Avoid numbering your explanations, but you may use numbered examples when necessary to clarify points. Do not use code block syntax for examples, use plain text instead. Do not try to make text bolder using "**bold text**" instead of this you can use dot or number. If u give an examples remeber to use double plain text before and after. At the end you can use some comparisons to explain in easier language with double plain text before',
       },
       {
         role: 'user',
-        content: `Could you explain this topic more clearly? Here is my question: ${question.content}. Please provide the answer in ${t('global:quizLanguage')}, you can use numbered examples or direct explanations as needed. Remeber the context of this question is ${question.topic}. for example in biology topic and question about blood functions your answer should look like this : Blood performs several functions in the body, including: 
+        content: `Could you explain this topic more clearly? Here is my question: ${question.content}. Please provide the answer in ${t('global:quizLanguage')}, you can use numbered examples or direct explanations as needed. Remember the context of this question is ${question.topic}. for example in biology topic and question about blood functions your answer should look like this : Blood performs several functions in the body, including: 
          
             - Transporting oxygen from the lungs to the body's tissues and organs, and transporting carbon dioxide from the body's tissues and organs back to the lungs.
             - Carrying nutrients such as glucose and amino acids from the digestive system to cells throughout the body.
@@ -53,7 +54,7 @@ export const Quiz = ({ questions, restartQuiz }: QuizProps) => {
             - Regulating body temperature by distributing heat produced by muscles during activity throughout the body.
             - Defending the body against infections and diseases by transporting white blood cells, antibodies, and other immune system components.
          
-        These functions help maintain the body's overall health and wellbeing by ensuring that all organs and tissues receive what they need and are protected from harm. Comparing blood to a delivery service in the body can make it easier to understand its role in keeping everything running smoothly. `,
+        These functions help maintain the body's overall health and wellbeing by ensuring that all organs and tissues receive what they need and are protected from harm. Comparing blood to a delivery service in the body can make it easier to understand its role in keeping everything running smoothly.`,
       },
     ],
   };
@@ -71,7 +72,9 @@ export const Quiz = ({ questions, restartQuiz }: QuizProps) => {
       .post('https://api.openai.com/v1/chat/completions', data, { headers: headers })
       .then((response) => {
         const apiAnswer = response.data.choices[0].message.content;
-        setExtendAnswer(apiAnswer);
+        const formattedAnswer = apiAnswer.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
+        const answerWithHr = formattedAnswer.replace(/```typescript([\s\S]*?)```/g, '<hr>$1<hr>');
+        setExtendAnswer(answerWithHr);
         setLoader(false);
       })
       .catch((error) => {
@@ -141,7 +144,10 @@ export const Quiz = ({ questions, restartQuiz }: QuizProps) => {
                   <div className="modal-box text-center">
                     <p className="py-4">{t('global:quiz:modalInfo')}</p>
                     {!loader ? (
-                      <p className="whitespace-pre-wrap break-words text-left">{extendAnswer}</p>
+                      <div
+                        className="whitespace-pre-wrap break-words text-left"
+                        dangerouslySetInnerHTML={{ __html: extendAnswer || '' }}
+                      />
                     ) : (
                       <span className="loading loading-dots loading-lg text-center"></span>
                     )}
