8c0bc945e347341b88696f4555ecae6006a5d7e0
Adrian <klimasa98@gmail.com>
2024-05-18 06:51:32 +0200

    add trash button to saved questions

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -36,7 +36,7 @@ export const Quiz = ({ questions, restartQuiz }: QuizProps) => {
   const { t } = useTranslation();
 
   const data = {
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4o',
     messages: [
       {
         role: 'system',
