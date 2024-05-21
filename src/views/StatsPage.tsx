f3a67f0b0dc42b9f2e800c756b15ac2029226dae
Adrian <klimasa98@gmail.com>
2024-05-21 20:48:45 +0200

    remove stats

--- src/views/StatsPage.tsx
+++ src/views/StatsPage.tsx
@@ -1,10 +1,10 @@
 import SavedQuestionsList from '../components/SavedQuestionsList/SavedQuestionsList';
-import Stats from '../components/Stats/Stats';
+
 
 export default function StatsPage() {
   return (
     <>
-      <Stats />
+
       <SavedQuestionsList />
     </>
   );
