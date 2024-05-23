342728cc1044ffe6556317196d9ea0473cb02772
Adrian <klimasa98@gmail.com>
2024-05-23 20:17:30 +0200

    add nav button

--- src/components/LearningGuide/Card/Card.tsx
+++ src/components/LearningGuide/Card/Card.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import styles from './Card.module.scss';
 import { learningCards } from '../../../constants/learningCards';
 
+
 export default function Card() {
   const [hoveredIndex, setHoveredIndex] = useState<number>(0);
   const [animateOut, setAnimateOut] = useState<boolean>(false);
@@ -24,7 +25,7 @@ export default function Card() {
   };
 
   return (
-    <div className="flex h-[400px] w-[600px] items-center justify-around">
+    <div className="flex h-[400px] w-[600px] items-center justify-around overflow-hidden ">
       <ul className="mt-16 flex h-[100%] flex-col gap-6 text-2xl">
         {learningCards.map((card, index) => (
           <li
@@ -32,15 +33,15 @@ export default function Card() {
             className={`cursor-pointer  ${hoveredIndex === index ? 'translate-x-5   text-primary transition-all' : ' transition-all'}`}
             onMouseEnter={() => onMouseInEvent(index)}
           >
-            0{index + 1} {card.title}
+            {index + 1} {card.title}
           </li>
         ))}
       </ul>
       <div
-        className={` flex h-[100%] w-[300px] rotate-3 flex-col rounded-2xl p-6 ${styles.card} ${animateOut ? styles.animateOut : styles.animateIn}`}
+        className={` flex h-[90%] w-[300px] rotate-3 flex-col rounded-2xl p-6 ${styles.card} ${animateOut ? styles.animateOut : styles.animateIn}`}
       >
         <h3 className="mb-5 text-xl font-[700] text-primary">{cardContent.title}</h3>
-        <p className="font-thin">{cardContent.description}</p>
+        <p className="leading-6">{cardContent.description}</p>
       </div>
     </div>
   );
--- src/components/LearningGuide/LearningGuide.tsx
+++ src/components/LearningGuide/LearningGuide.tsx
@@ -1,12 +1,19 @@
+import { Link } from 'react-router-dom';
 import { Wrapper } from '../MainLayout/Wrapper/Wrapper';
 import Card from './Card/Card';
 
 export default function LearningGuide() {
   return (
     <>
-      <section id="how-to-learn" className=" flex h-[70vh]  items-start ">
+      <section id="how-to-learn" className=" flex h-[70vh]  items-start relative ">
         <Wrapper>
-        <Card/>
+          <div className='flex flex-col'>
+
+          <Card />
+          <Link className=" btn btn-primary self-center justify-self-center mt-10 h-[70px] w-[250px] " to={'/form'}>
+            Stwórz swój quiz
+          </Link>
+          </div>
         </Wrapper>
       </section>
     </>
