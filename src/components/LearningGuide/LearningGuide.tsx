b352ae79b47a8106ebfefc25f2a9023394b1a92d
Adrian <klimasa98@gmail.com>
2024-05-27 22:44:35 +0200

    add new btn for landing page

--- src/components/LearningGuide/LearningGuide.tsx
+++ src/components/LearningGuide/LearningGuide.tsx
@@ -1,16 +1,19 @@
-
 import { Wrapper } from '../MainLayout/Wrapper/Wrapper';
 import Card from './Card/Card';
+import { NavLink } from 'react-router-dom';
+
+import { useTranslation } from 'react-i18next';
 
 export default function LearningGuide() {
+
+  const {t} = useTranslation()
+
   return (
     <>
-      <section id="how-to-learn" className=" relative flex  h-[70vh] items-start ">
+      <section id="how-to-learn" className=" relative flex  h-[70vh] items-start mb-20 ">
         <Wrapper>
-          <div className="flex flex-col">
-            <Card />
-           
-          </div>
+          <Card />
+          <NavLink className='btn btn-primary mt-10 w-[100%] ' to={'/form'}>{ t('global:nav:createNewQuiz')}</NavLink>
         </Wrapper>
       </section>
     </>
