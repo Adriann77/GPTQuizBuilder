ce8e7128ce08dbab7977b40590b750986240afa3
Adrian <klimasa98@gmail.com>
2024-05-31 16:53:05 +0200

    change model

--- src/components/QuizFetcher/QuizFetcher.tsx
+++ src/components/QuizFetcher/QuizFetcher.tsx
@@ -36,7 +36,7 @@ export const QuizFetcher = ({ data, restartQuiz }: { data: any; restartQuiz: any
           const response = await axios.post(
             `https://api.openai.com/v1/chat/completions`,
             {
-              model: 'gpt-3.5-turbo',
+              model: 'gpt-4o',
               messages: [
                 {
                   role: 'user',
